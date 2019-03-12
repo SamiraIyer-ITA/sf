593f7f64daaa46461667c77ce3977740bb4de4f9
Cary Freeman <cary.freeman@trade.gov>
2019-03-11 22:12:28 -0500

    Updated for comp with v43 and higher

--- src/aura/Base/BaseHelper.js
+++ src/aura/Base/BaseHelper.js
@@ -1,302 +1,293 @@
 ({
-    isLightningExperience: function() {
+	isLightningExperience: function() {
 
-        var toast = $A.get("e.force:showToast");
-        if (toast){
-            return true;
-        } else {
-            return false;
-        }
-    },
-    showNotification: function(component,header,title,message,variant,closeCallback) {
+		var toast = $A.get("e.force:showToast");
+		if (toast){
+			return true;
+		} else {
+			return false;
+		}
+	},
+	showNotification: function(component,header,title,message,variant,closeCallback) {
 
-        if (this.isLightningExperience()) {
-            var compEvent = component.getEvent("BaseNotification");
+		if (this.isLightningExperience()) {
+			var compEvent = component.getEvent("BaseNotification");
 
-            variant = variant || "info";
+			variant = variant || "info";
 
-            compEvent.setParams({
-                type : 'notification',
-                config: {
-                    header: header,
-                    title: title,
-                    message: message,
-                    variant: variant,
-                    closeCallback: closeCallback
-                }
-            });
-            compEvent.fire();
-        }
+			compEvent.setParams({
+				type : 'notification',
+				config: {
+					header: header,
+					title: title,
+					message: message,
+					variant: variant,
+					closeCallback: closeCallback
+				}
+			});
+			compEvent.fire();
+		}
 
-    },
+	},
 
-    showToast: function(title, duration, message,messageTemplate, messageTemplateData, toastType,mode) {
+	showToast: function(title, duration, message,messageTemplate, messageTemplateData, toastType,mode) {
 
-        if (this.isLightningExperience()) {
-            var toastEvent = $A.get("e.force:showToast");
+		if (this.isLightningExperience()) {
+			var toastEvent = $A.get("e.force:showToast");
 
-            toastType = toastType || "info";
-            mode = mode || "sticky";
-            message = message || "Unknown message";
-            duration = duration || 5000;
+			toastType = toastType || "info";
+			mode = mode || "sticky";
+			message = message || "Unknown message";
+			duration = duration || 5000;
 
-            toastEvent.setParams({
-                title: title,
-                duration: duration,
-                message: message,
-                messageTemplate: messageTemplate,
-                messageTemplateData: messageTemplateData,
-                type: toastType,
-                mode: mode
-            });
+			toastEvent.setParams({
+				title: title,
+				duration: duration,
+				message: message,
+				messageTemplate: messageTemplate,
+				messageTemplateData: messageTemplateData,
+				type: toastType,
+				mode: mode
+			});
 
-            toastEvent.fire();
-        }
+			toastEvent.fire();
+		}
 
-    },
+	},
 
-    handleErrors : function(errors) {
+	handleErrors : function(errors) {
 
-        //Populate the message
-        var message = "";
+		//Populate the message
+		var message = "";
 
-        for(var i=0; i < errors.length; i++) {
-            for(var j=0; errors[i].pageErrors && j < errors[i].pageErrors.length; j++) {
-                message += (message.length > 0 ? '\n' : '') + errors[i].pageErrors[j].message;
-            }
-            if(errors[i].fieldErrors) {
-                for(var fieldError in errors[i].fieldErrors) {
-                    var thisFieldError = errors[i].fieldErrors[fieldError];
-                    for(var j=0; j < thisFieldError.length; j++) {
-                        message += (message.length > 0 ? '\n' : '') + thisFieldError[j].message;
-                    }
-                }
-            }
-            if(errors[i].message) {
-                message += (message.length > 0 ? '\n' : '') + errors[i].message;
-            }
-        }
+		for(var i=0; i < errors.length; i++) {
+			for(var j=0; errors[i].pageErrors && j < errors[i].pageErrors.length; j++) {
+				message += (message.length > 0 ? '\n' : '') + errors[i].pageErrors[j].message;
+			}
+			if(errors[i].fieldErrors) {
+				for(var fieldError in errors[i].fieldErrors) {
+					var thisFieldError = errors[i].fieldErrors[fieldError];
+					for(var j=0; j < thisFieldError.length; j++) {
+						message += (message.length > 0 ? '\n' : '') + thisFieldError[j].message;
+					}
+				}
+			}
+			if(errors[i].message) {
+				message += (message.length > 0 ? '\n' : '') + errors[i].message;
+			}
+		}
 
-        this.showToast("Error", 0, message, null, null, "error","sticky")
-    },
-
-
-    callServer : function(component,method,callback,params,storable) {
-
-        var compEvent = component.getEvent("showSpinner");
-        compEvent.fire();
+		this.showToast("Error", 0, message, null, null, "error","sticky")
+	},
 
 
-        var action = component.get(method);
-        if (params) {
-            action.setParams(params);
-        }
+	callServer : function(component,method,callback,params) {
 
-        if (storable) {
-            action.setStorable();
-        } else {
-            action.setStorable({
-                "ignoreExisting": "true"
-            });
+		var compEvent = component.getEvent("showSpinner");
+		compEvent.fire();
 
-        }
 
-        action.setCallback(this,function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                // pass returned value to callback function
-                callback.call(this,response.getReturnValue());
-            } else if (state === "ERROR") {
-                // generic error handler
-                var errors = response.getError();
-                if (errors) {
-                    console.error("Errors", errors);
-                    this.handleErrors(errors);
-                } else {
-                    this.handleErors(new Error("Unknown Error"));
-                }
-            }
-            var compEvent = component.getEvent("hideSpinner");
-            compEvent.fire();
+		var action = component.get(method);
+		if (params) {
+			action.setParams(params);
+		}
 
-        });
+		action.setCallback(this,function(response) {
+			var state = response.getState();
+			if (state === "SUCCESS") {
+				// pass returned value to callback function
+				callback.call(this,response.getReturnValue());
+			} else if (state === "ERROR") {
+				// generic error handler
+				var errors = response.getError();
+				if (errors) {
+					console.error("Errors", errors);
+					this.handleErrors(errors);
+				} else {
+					this.handleErors(new Error("Unknown Error"));
+				}
+			}
+			var compEvent = component.getEvent("hideSpinner");
+			compEvent.fire();
 
-        $A.enqueueAction(action);
-    },
+		});
 
-    hasValidData: function(component, fields) {
-        var field = "";
-        var returnValue = false;
+		$A.enqueueAction(action);
+	},
 
-        for (var i=0; i<fields.length; i++) {
-            //console.log("Checking field " + fields[i]);
-            field = component.find(fields[i]);
-            /*if (!field) {
-               console.log("Unable to find field " + fields[i]);
-            }*/
-            if (Array.isArray(field)) {
-                //console.log("Array of fields.  Array size is " + field.length);
-                //If there is more than 1 component using the same aura:id, find() will return an array of them
-                for (var x = 0; x<field.length; x++) {
-                    returnValue = this.checkFieldValidity(field[x]);
-                    //console.log("Checking validity of " + field[i] + " " + returnValue);
-                    if (!returnValue) {
-                        //console.log("Returning false");
-                        return false;
-                    }
-                }
-            } else {
-                returnValue = this.checkFieldValidity(field);
-                //console.log("Checking validity of " + fields[i] + " " + returnValue);
-                if (!returnValue) {
-                    return false;
-                }
-            }
-        }
+	hasValidData: function(component, fields) {
+		var field = "";
+		var returnValue = false;
 
-        //console.log("Returning true because all fields are valid");
+		for (var i=0; i<fields.length; i++) {
+			//console.log("Checking field " + fields[i]);
+			field = component.find(fields[i]);
+			/*if (!field) {
+			   console.log("Unable to find field " + fields[i]);
+			}*/
+			if (Array.isArray(field)) {
+				//console.log("Array of fields.  Array size is " + field.length);
+				//If there is more than 1 component using the same aura:id, find() will return an array of them
+				for (var x = 0; x<field.length; x++) {
+					returnValue = this.checkFieldValidity(field[x]);
+					//console.log("Checking validity of " + field[i] + " " + returnValue);
+					if (!returnValue) {
+						//console.log("Returning false");
+						return false;
+					}
+				}
+			} else {
+				returnValue = this.checkFieldValidity(field);
+				//console.log("Checking validity of " + fields[i] + " " + returnValue);
+				if (!returnValue) {
+					return false;
+				}
+			}
+		}
 
-        return true;
-    },
+		//console.log("Returning true because all fields are valid");
 
-    validateRichTextFieldValueAndLength: function(component, fieldName, maxLength) {
-        var richTextField = component.find(fieldName);
-        /*if (!richTextField) {
-            console.log("No value for richTextField");
-        }*/
+		return true;
+	},
 
-        if (Array.isArray(richTextField)) {
-            //If there is more than 1 component using the same aura:id, find() will return an array of them
-            for (var x = 0; x<richTextField.length; x++) {
-                if (richTextField[x]) {
-                    var richTextFieldValue = richTextField[x].get("v.value");
-                    if (!richTextFieldValue || richTextFieldValue.length > maxLength) {
-                        //Make sure the field has a value and is less than maxLength characters
-                        richTextField[x].set("v.valid", false);
-                        return false;
-                    } else {
-                        richTextField[x].set("v.valid", true);
-                    }
-                }
-            }
-            return true;
-        } else {
-            var richTextFieldValue = richTextField.get("v.value");
-            if (!richTextFieldValue || richTextFieldValue.length > maxLength) {
-                //Make sure the field has a value and is less than maxLength characters
-                richTextField.set("v.valid", false);
-                return false;
-            } else {
-                richTextField.set("v.valid", true);
-                return true;
-            }
-        }
+	validateRichTextFieldValueAndLength: function(component, fieldName, maxLength) {
+		var richTextField = component.find(fieldName);
+		/*if (!richTextField) {
+			console.log("No value for richTextField");
+		}*/
 
-        return false;
-    },
+		if (Array.isArray(richTextField)) {
+			//If there is more than 1 component using the same aura:id, find() will return an array of them
+			for (var x = 0; x<richTextField.length; x++) {
+				if (richTextField[x]) {
+					var richTextFieldValue = richTextField[x].get("v.value");
+					if (!richTextFieldValue || richTextFieldValue.length > maxLength) {
+						//Make sure the field has a value and is less than maxLength characters
+						richTextField[x].set("v.valid", false);
+						return false;
+					} else {
+						richTextField[x].set("v.valid", true);
+					}
+				}
+			}
+			return true;
+		} else {
+			var richTextFieldValue = richTextField.get("v.value");
+			if (!richTextFieldValue || richTextFieldValue.length > maxLength) {
+				//Make sure the field has a value and is less than maxLength characters
+				richTextField.set("v.valid", false);
+				return false;
+			} else {
+				richTextField.set("v.valid", true);
+				return true;
+			}
+		}
 
-    validateOnlyOneHasValue: function(component, fields) {
-        var field;
-        var fieldValue;
-        var oneHasValue = false;
+		return false;
+	},
 
-        for (var i=0; i<fields.length; i++) {
-            field = component.find(fields[i]);
-            if (field) {
-                //The field may not be displayed on the screen, so only check for validity if the field can be seen
-                if (Array.isArray(field)) {
-                    //If there is more than 1 component using the same aura:id, find() will return an array of them
-                    for (var x = 0; x < field.length; x++) {
-                        if (field[x]) {
-                            fieldValue = field[x].get("v.value");
-                            if (fieldValue) {
-                                if (oneHasValue) {
-                                    return false;  //Too many fields have a value
-                                }
-                                oneHasValue = true;
-                            }
-                        }
-                    }
-                } else {
-                    fieldValue = field.get("v.value");
-                    if (fieldValue) {
-                        if (oneHasValue) {
-                            return false;  //Too many fields have a value
-                        }
-                        oneHasValue = true;
-                    }
-                }
-            }
-        }
+	validateOnlyOneHasValue: function(component, fields) {
+		var field;
+		var fieldValue;
+		var oneHasValue = false;
 
-        return oneHasValue;
-    },
+		for (var i=0; i<fields.length; i++) {
+			field = component.find(fields[i]);
+			if (field) {
+				//The field may not be displayed on the screen, so only check for validity if the field can be seen
+				if (Array.isArray(field)) {
+					//If there is more than 1 component using the same aura:id, find() will return an array of them
+					for (var x = 0; x < field.length; x++) {
+						if (field[x]) {
+							fieldValue = field[x].get("v.value");
+							if (fieldValue) {
+								if (oneHasValue) {
+									return false;  //Too many fields have a value
+								}
+								oneHasValue = true;
+							}
+						}
+					}
+				} else {
+					fieldValue = field.get("v.value");
+					if (fieldValue) {
+						if (oneHasValue) {
+							return false;  //Too many fields have a value
+						}
+						oneHasValue = true;
+					}
+				}
+			}
+		}
 
-    checkFieldValidity: function(field) {
-        if (!field) {
-            //The field may not be displayed on the screen, so only check for validity if the field can be seen
-            //console.log("Field not found");
-            return true;
-        }
-        var validity = field.get("v.validity");
-        if (typeof validity == 'object') {
-            //console.log(validity);
-            var isValid = true;
+		return oneHasValue;
+	},
 
-            if (validity.badInput) {
-                field.set('v.validity', {valid:false, badInput :true});
-                isValid = false;
-            }
+	checkFieldValidity: function(field) {
+		if (!field) {
+			//The field may not be displayed on the screen, so only check for validity if the field can be seen
+			//console.log("Field not found");
+			return true;
+		}
+		var validity = field.get("v.validity");
+		if (typeof validity == 'object') {
+			//console.log(validity);
+			var isValid = true;
 
-            if (validity.patternMismatch) {
-                field.set('v.validity', {valid:false, patternMismatch :true});
-                isValid = false;
-            }
+			if (validity.badInput) {
+				field.set('v.validity', {valid:false, badInput :true});
+				isValid = false;
+			}
 
-            if (validity.rangeOverflow) {
-                field.set('v.validity', {valid:false, rangeOverflow :true});
-                isValid = false;
-            }
+			if (validity.patternMismatch) {
+				field.set('v.validity', {valid:false, patternMismatch :true});
+				isValid = false;
+			}
 
-            if (validity.rangeUnderflow) {
-                field.set('v.validity', {valid:false, rangeUnderflow :true});
-                isValid = false;
-            }
+			if (validity.rangeOverflow) {
+				field.set('v.validity', {valid:false, rangeOverflow :true});
+				isValid = false;
+			}
 
-            if (validity.stepMismatch) {
-                field.set('v.validity', {valid:false, stepMismatch :true});
-                isValid = false;
-            }
+			if (validity.rangeUnderflow) {
+				field.set('v.validity', {valid:false, rangeUnderflow :true});
+				isValid = false;
+			}
 
-            if (validity.tooLong) {
-                field.set('v.validity', {valid:false, tooLong :true});
-                isValid = false;
-            }
+			if (validity.stepMismatch) {
+				field.set('v.validity', {valid:false, stepMismatch :true});
+				isValid = false;
+			}
 
-            if (validity.typeMismatch) {
-                field.set('v.validity', {valid:false, typeMismatch :true});
-                isValid = false;
-            }
+			if (validity.tooLong) {
+				field.set('v.validity', {valid:false, tooLong :true});
+				isValid = false;
+			}
 
-            if (validity.valueMissing) {
-                field.set('v.validity', {valid:false, valueMissing :true});
-                isValid = false;
-            }
+			if (validity.typeMismatch) {
+				field.set('v.validity', {valid:false, typeMismatch :true});
+				isValid = false;
+			}
 
-            if (validity.valid) {
-                return isValid;
-            }
+			if (validity.valueMissing) {
+				field.set('v.validity', {valid:false, valueMissing :true});
+				isValid = false;
+			}
 
-            field.showHelpMessageIfInvalid();
+			if (validity.valid) {
+				return isValid;
+			}
 
-            return false;
-        } else {
-            return true;
-        }
+			field.showHelpMessageIfInvalid();
 
-        //console.error("Unable to find Validity");
+			return false;
+		} else {
+			return true;
+		}
 
-        return false;
-    }
+		//console.error("Unable to find Validity");
+
+		return false;
+	}
 
 })
