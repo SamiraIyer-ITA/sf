6f1c1a5a351e754db9f17d945c2b3881830be38a
Cary Freeman <cary.freeman@trade.gov>
2020-04-06 10:33:23 -0500

    Removed console.log

--- src/aura/PaymentDetails/PaymentDetailsController.js
+++ src/aura/PaymentDetails/PaymentDetailsController.js
@@ -8,7 +8,6 @@
 			cmp,
 			"c.getPaymentDetails",
 			function (response) {
-				console.log(JSON.stringify(response));
 				//Update the card with the returned data
 				cmp.set("v.paymentDetailsController", response);
 			},
