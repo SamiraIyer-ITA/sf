ed16d9edbf745ab09833e6563a68903ae9b36c23
danielharoTrade <daniel.haro@trade.gov>
2020-11-03 15:08:37 -0600

    SM-10124 Aura cmp and test class

---
+++ src/aura/Oppty_Past_Due_Component/Oppty_Past_Due_ComponentController.js
@@ -0,0 +1,21 @@
+({
+	doInit : function(component, event, helper) {
+		var action = component.get("c.getStatus");
+		action.setParams({
+            "opptyRecordId": component.get("v.recordId")
+        });
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                component.set("v.showMsg", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+    
+        // Send action off to be executed
+        $A.enqueueAction(action);
+	}
+})
